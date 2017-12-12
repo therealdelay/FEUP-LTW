11c0c628824815583a35e641f7a8670a42be2ac9
Bruno Piedade <dako1oblivion@hotmail.com>
2017-12-12 18:28:44 +0000

    Small changes to time.php and profile_page.css (delete button)

--- Project 1/templates/todos/time.php
+++ Project 1/templates/todos/time.php
@@ -2,13 +2,14 @@
 	
 	function getTimeDiff($ref){
 		$currdt = new DateTime("now");
-		$refdt = new DateTime($ref);
+		$refdt = new DateTime($ref." +1 day");
 		
 		$days = $currdt->diff($refdt)->format('%R%a');
-		//echo $days;
  		$resMsg;
 		if($days > 0)
 			$resMsg = intval($days) . " day(s) remaining";
+		else if($days == 0 && (strrpos($days,"+") !== false))
+			$resMsg = "Last day";
 		else
 			$resMsg = "Overtime";
 		return $resMsg;
